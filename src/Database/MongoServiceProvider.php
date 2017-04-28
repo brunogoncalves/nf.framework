bb571862118bcf0e2265388f3b250aa1039c71e6
Bruno Goncalves <bugotech@gmail.com>
2017-04-28 07:46:09 -0300

    config mongo

--- src/Database/MongoServiceProvider.php
+++ src/Database/MongoServiceProvider.php
@@ -15,21 +15,5 @@ class MongoServiceProvider extends ServiceProvider
         db()->extend('mongodb', function ($config) {
             return new \Jenssegers\Mongodb\Connection($config);
         });
-
-        // Register connection
-        $cons = config()->get('database.connections', []);
-        $cons['mongo'] = [
-            'driver' => 'mongodb',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'options' => [
-                'database' => 'admin'
-            ]
-        ];
-
-        config()->set('database.connections', $cons);
     }
 }
